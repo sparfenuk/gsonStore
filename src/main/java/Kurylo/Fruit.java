eb8420795c46cf01d49dc1dd5ba9822db6daf7bc
Serhii <serg14581@gmail.com>
2019-04-22 21:13:42 +0300

    fruit

---
+++ src/main/java/Kurylo/Fruit.java
@@ -0,0 +1,11 @@
+package Kurylo;
+
+import java.util.Date;
+
+public class Fruit {
+
+    private enum FRUIT_TYPE{};
+    private  int expirationDays;
+    private Date deliveryDate;
+    private double price;
+}
