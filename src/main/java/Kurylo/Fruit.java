8fc8b87be5319476566b4dd2739f683341a9b65b
Serhii <serg14581@gmail.com>
2019-04-22 21:15:45 +0300

    rename type

--- src/main/java/Kurylo/Fruit.java
+++ src/main/java/Kurylo/Fruit.java
@@ -4,8 +4,32 @@ import java.util.Date;
 
 public class Fruit {
 
-    private enum FRUIT_TYPE{};
+    private enum Type{};
     private  int expirationDays;
     private Date deliveryDate;
     private double price;
+
+    public int getExpirationDays() {
+        return expirationDays;
+    }
+
+    public void setExpirationDays(int expirationDays) {
+        this.expirationDays = expirationDays;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
