4a143c47c6653ff7e9dd8c364fee068eaea11636
pacman_n <sparfenuk13@gmail.com>
2019-04-22 21:27:11 +0300

    Type

---
+++ src/main/java/Type.java
@@ -0,0 +1,18 @@
+public enum Type {
+    Pineapple("Pineapple"),
+    Aplle("Aplle"),
+    Strawberry("Strawberry"),
+    Orange("Orange"),
+    Blueberry("Blueberry"),
+    Lemon("Lemon"),
+    Lime("Lime"),
+    Grape("Grape");
+
+    public String fruit;
+
+    Type(String fruit){
+        this.fruit = fruit;
+    }
+
+
+}
