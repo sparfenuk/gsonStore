f05478da45523ce2c4e60da6a6c637c06c9b2f8c
kalchenia <vladuk383@gmail.com>
2019-04-22 21:35:26 +0300

    Shop

---
+++ src/main/java/Shop.java
@@ -0,0 +1,16 @@
+import Kurylo.Fruit;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+    ArrayList<Fruit> fruits = new ArrayList<>();
+
+    public void addFruits(String pathToJsonFile)
+    {
+
+
+    }
+
+}
